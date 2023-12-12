bb5cbdfd842e7c9bf5f91e5fc2e7728b0eeb89bb
tomer-mobb <132216976+tomer-mobb@users.noreply.github.com>
2023-12-12 17:50:23 +0700

    Update xss-example.js

--- xss-example.js
+++ xss-example.js
@@ -1,5 +1,5 @@
 var urlParams = new URLSearchParams(window.location.search);
-var username = urlParams.get('username');
+var username = urlParams.get('username'); // bla
 
 var unsafe_div = window.document.getElementById("vulnerable-div");
 unsafe_div.innerHTML = "Hello to you, " + username + "!!!!";
